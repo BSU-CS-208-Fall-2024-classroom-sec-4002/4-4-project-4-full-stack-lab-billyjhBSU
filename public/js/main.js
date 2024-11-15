a15a8cb505a167e0b040df8c149aabec033bfd8e
William Hoffert <billyhoffert@u.boisestate.edu>
2024-11-14 20:48:45 -0700

    Finished project 4

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,43 @@
-// TODO: Add any JavaScript here to make your page fancy :)
+import { Database } from "sqlite3";
+
+document.addEventListener('DOMContentLoaded', function () {
+    const todoForm = document.getElementById('todoForm');
+    const todoList = document.getElementById('todoList');
+    let itemCount = 0;
+    
+    // Function to add a new TODO item
+    todoForm.addEventListener('submit', function (e) {
+        const itemInput = document.getElementById('item');
+        const itemText = itemInput.value.trim();
+        
+        if (itemText !== '') {
+            itemCount++;
+            const listItem = document.createElement('li');
+
+            // Create unique id for each item
+            const itemID = `todo-item-${itemCount}`;
+            listItem.id = itemID;
+            
+            // Item paragraph
+            const itemParagraph = document.createElement('p');
+            itemParagraph.textContent = `${itemCount}: ${itemText}`;
+            
+            // Delete button
+            const deleteButton = document.createElement('button');
+            deleteButton.textContent = 'delete';
+            deleteButton.addEventListener('click', function () {
+                todoList.removeChild(listItem);
+            });
+            
+            // Append text and delete button to the list item
+            listItem.appendChild(itemParagraph);
+            listItem.appendChild(deleteButton);
+            
+            // Append the list item to the TODO list
+            todoList.appendChild(listItem);
+            
+            // Clear the input field
+            itemInput.value = '';
+        }
+    });
+  });
--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 import express from 'express'
 import sql from 'sqlite3'
-
 const sqlite3 = sql.verbose()
 
 // Create an in memory table to use
@@ -11,31 +10,50 @@ const db = new sqlite3.Database(':memory:')
 db.run(`CREATE TABLE todo (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     task TEXT NOT NULL)`)
-
+   
 const app = express()
 app.use(express.static('public'))
 app.set('views', 'views')
 app.set('view engine', 'pug')
 app.use(express.urlencoded({ extended: false }))
 
+// Get
 app.get('/', function (req, res) {
-    //TODO You will need to do a SQL select here
-    //TODO You will need to update the code below!
-    console.log('GET called')
-    res.render('index')
-
+    db.all('SELECT id, task FROM todo', function(err, rows) {
+        if (err) {
+            console.error(err)
+            return res.status(500).send('Database error occurred')
+        }
+        res.render('index', { tasks: rows })
+    })
 })
 
+// Add
 app.post('/', function (req, res) {
     console.log('adding todo item')
-    //TODO You will need to to do a SQL Insert here
-
+    const stmt = db.prepare('INSERT INTO todo (task) VALUES (?)')
+    stmt.run(req.body.todo, function(err) {
+        if (err) {
+            console.error(err)
+            return res.status(500).send('Error adding todo item')
+        }
+        res.redirect('/')
+    })
+    stmt.finalize()
 })
 
+// Delete
 app.post('/delete', function (req, res) {
     console.log('deleting todo item')
-    //TODO you will need to delete here
-
+    const stmt = db.prepare('DELETE FROM todo WHERE id = ?')
+    stmt.run(req.body.id, function(err) {
+        if (err) {
+            console.error(err)
+            return res.status(500).send('Error deleting todo item')
+        }
+        res.redirect('/')
+    })
+    stmt.finalize()
 })
 
 // Start the web server
